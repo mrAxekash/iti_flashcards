14647bd5261134d204ff1073ef9ed1f509b6676a
Briws <brightwiths@gmail.com>
2023-08-23 23:17:47 +0400

    select with render, arguments works

--- src/src/components/ui/Select/Select.stories.tsx
+++ src/src/components/ui/Select/Select.stories.tsx
@@ -1,26 +1,14 @@
 import { useState } from 'react'
 
-import { Meta, StoryFn, StoryObj } from '@storybook/react'
+import { Meta, StoryObj } from '@storybook/react'
 
 // import {OptionType, Select} from '@/src/components/ui/Select/Select.tsx'
-import { OptionType, Select, SelectPropsType } from './Select.tsx'
-
-const DecoratorFn = (Story: StoryFn<SelectPropsType>) => {
-  const [value, setValue] = useState('Select-box')
-  const arr: Array<OptionType> = [
-    { id: 1, value: 'value 1' },
-    { id: 2, value: 'value 2' },
-    { id: 3, value: 'value 3' },
-  ]
-
-  return <Story options={arr} value={value} onChangeOption={setValue} />
-}
+import { OptionType, Select } from './Select.tsx'
 
 const meta = {
   title: 'Components/Select',
   component: Select,
   tags: ['autodocs'],
-  decorators: [DecoratorFn],
   argTypes: {
     isDisabled: {
       options: [true, false],
@@ -32,6 +20,17 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
+  render: args => {
+    const [value, setValue] = useState('Select-box')
+    const arr: Array<OptionType> = [
+      { id: 1, value: 'value 1' },
+      { id: 2, value: 'value 2' },
+      { id: 3, value: 'value 3' },
+    ]
+
+    return <Select {...args} options={arr} value={value} onChangeOption={setValue} />
+  },
+
   args: {
     isDisabled: false,
     value: '',
