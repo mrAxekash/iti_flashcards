01cdd592f30208728ee8f1dd22aad240b56a3143
Ekaterina <ushakova.esu@gmail.com>
2023-08-25 21:29:36 +0600

    Checkbox

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
+
 import { Button } from './src/components/ui'
 
+import { Checkbox } from '@/src/components/ui/Checkbox/checkbox.tsx'
+
 export function App() {
+  let [checked, setCheked] = useState(true)
+
   return (
     <div>
       <Button as={'button'}>Text</Button>
+      <Checkbox label="some label" checked={checked} onChange={setCheked} disabled={true} />
+      <Checkbox label="Check-box" disabled={false} />
     </div>
   )
 }
