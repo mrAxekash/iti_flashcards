dc1c8d86a71a5573961ad986e994c491817c780e
mrAxekash <aliaxandr_ku@mail.ru>
2023-08-29 01:34:33 +0300

    feature/#8-Input/Textfield part 6

--- src/src/components/ui/Textfield/Textfield.stories.ts
+++ src/src/components/ui/Textfield/Textfield.stories.ts
@@ -25,32 +25,53 @@ export const Password: Story = {
     label: 'Input Pasword',
   },
 }
-// export const Search: Story = {
-//   args: {
-//     type: 'search',
-//   },
-// }
+export const Search: Story = {
+  args: {
+    type: 'search',
+  },
+}
 
-// export const ErrorText: Story = {
-//   args: {
-//     type: 'text',
-//     label: 'Input search',
-//     errorMessage: 'Some error',
-//   },
-// }
+export const ErrorText: Story = {
+  args: {
+    type: 'text',
+    label: 'Input search',
+    errorMessage: 'Text error!',
+  },
+}
 
-// export const ErrorSearch: Story = {
-//   args: {
-//     type: 'search',
-//     label: 'Input search',
-//     errorMessage: 'Incorrect request',
-//   },
-// }
+export const ErrorSearch: Story = {
+  args: {
+    type: 'search',
+    label: 'Input search',
+    errorMessage: 'Incorrect request!',
+  },
+}
 
-// export const ErrorPassword: Story = {
-//   args: {
-//     type: 'password',
-//     label: 'Input search',
-//     errorMessage: 'Incorrect password',
-//   },
-// }
+export const ErrorPassword: Story = {
+  args: {
+    type: 'password',
+    label: 'Input search',
+    errorMessage: 'Incorrect password!',
+  },
+}
+export const DisabledText: Story = {
+  args: {
+    type: 'text',
+    label: 'Disabled search',
+    disabled: true,
+  },
+}
+export const DisabledPassword: Story = {
+  args: {
+    type: 'password',
+    label: 'Disabled password',
+    disabled: true,
+  },
+}
+export const DisabledSearch: Story = {
+  args: {
+    type: 'search',
+    label: 'Disabled search',
+    disabled: true,
+  },
+}
--- src/src/components/ui/Textfield/Textfield.tsx
+++ src/src/components/ui/Textfield/Textfield.tsx
@@ -7,80 +7,25 @@ import textfieldStyle from './Textfield.module.scss'
 import { Eye } from '@/src/assets/icons/Eye.tsx'
 import { InvisibilityOff } from '@/src/assets/icons/InvisibilityOff.tsx'
 import { Search } from '@/src/assets/icons/Search.tsx'
-//import { Search } from '@/src/assets/icons/Search.tsx'
-
-// type InputPropsType<E extends ElementType = 'input'> = {
-//   label?: string
-//   type: 'text' | 'password' | 'search'
-//   errorMessage?: string | null
-//   isDisabled?: boolean
-//   as?: E
-//   onChangeValue?: (value: string) => void
-// } & ComponentPropsWithoutRef<E>
-
-// export const Textfield = <E extends ElementType = 'input'>(
-//   props: InputPropsType & Omit<ComponentPropsWithoutRef<E>, keyof InputPropsType<E>>
-// ) => {
-//   const { label, type, errorMessage, isDisabled, as: Component = 'input', ...rest } = props
-//
-//   const [eyeState, setEyeState] = useState(false)
-//
-//   const finalType = getFinalType(type, eyeState)
-//   const eyeChangeHandler = () => {
-//     setEyeState(!eyeState)
-//   }
-//
-//   return (
-//     <div className={` ${inputStyle[type]}`}>
-//       <label
-//         className={`${
-//           type === 'text' || type === 'password' ? inputStyle.primaryTitle : inputStyle.deleteField
-//         } ${isDisabled && inputStyle.disabled} `}
-//       >
-//         {label}
-//       </label>
-//       <div
-//         className={`${type === 'search' && inputStyle.iconPassword} ${
-//           type === 'password' && inputStyle.iconPassword
-//         }`}
-//       >
-//         {type === 'password' && (
-//           <button
-//             onClick={eyeChangeHandler}
-//             className={`${inputStyle.button} ${isDisabled && inputStyle.disabled}`}
-//             disabled={isDisabled}
-//           >
-//             {eyeState && <Eye disabled={isDisabled} />}
-//             {!eyeState && <InvisibilityOff disabled={isDisabled} />}
-//           </button>
-//         )}
-//         {type === 'search' && <Search />}
-//         <Component
-//           type={finalType}
-//           className={` ${errorMessage && inputStyle.error} ${inputStyle.defaultInput} ${
-//             type === 'search' && inputStyle.searchInput
-//           }
-//             ${type === 'password' && inputStyle.passwordInput}
-//
-//           }`}
-//           disabled={isDisabled}
-//           {...rest}
-//         />
-//         {errorMessage && <div className={inputStyle.error}>{errorMessage}</div>}
-//       </div>
-//     </div>
-//   )
-// }
-////////////////////////////////////////////////////////////////////////////////////
 
 export type TextfieldPropsType = {
-  // onChangeValue?: () => void
-  value?: string
+  onChangeValue?: (value: string) => void
   errorMessage?: string
   label?: string
+  labelProps?: ComponentProps<'label'>
+  containerProps?: ComponentProps<'div'>
 } & ComponentPropsWithoutRef<'input'>
 export const Textfield = (props: TextfieldPropsType) => {
-  const { label, errorMessage, type, value, ...rest } = props
+  const {
+    label,
+    errorMessage,
+    type,
+    onChangeValue,
+    labelProps,
+    containerProps,
+    disabled,
+    ...rest
+  } = props
   const [showPassword, setShowPassword] = useState(false)
 
   const isShowButtonPasswordShowed = type === 'password'
@@ -89,27 +34,51 @@ export const Textfield = (props: TextfieldPropsType) => {
   const finalType = getFinalType(type, showPassword)
 
   const classNames = {
-    container: clsx(textfieldStyle.container),
+    container: clsx(textfieldStyle.container, containerProps?.className),
     wrapper: clsx(textfieldStyle.inputWrapper),
-    button: clsx(textfieldStyle.button),
-    input: clsx(textfieldStyle.input),
+    button: clsx(
+      type === 'password' && textfieldStyle.button,
+      type === 'search' && textfieldStyle.buttonSearch
+    ),
+    inputField: clsx(
+      textfieldStyle.inputField,
+      type === 'search' && textfieldStyle.inputFieldSearch,
+      errorMessage && textfieldStyle.inputError
+    ),
+    label: clsx(textfieldStyle.label, disabled && textfieldStyle.disabled, labelProps?.className),
+    buttonSearch: clsx(textfieldStyle.buttonSearch),
+    subField: clsx(textfieldStyle.subField, textfieldStyle.error),
   }
 
   return (
     <div className={classNames.container}>
       {/*Add Textarea component for label*/}
-      {label && <label> {label}</label>}
+      {label && <label className={classNames.label}> {label}</label>}
       <div className={classNames.wrapper}>
-        <input type={finalType} value={value} {...rest} className={classNames.input} />
+        <input
+          type={finalType}
+          className={classNames.inputField}
+          placeholder={'Hello'}
+          disabled={disabled}
+          {...rest}
+        />
         {isShowButtonPasswordShowed && (
-          <button onClick={() => setShowPassword(!showPassword)} className={classNames.button}>
-            {showPassword ? <InvisibilityOff /> : <Eye />}
+          <button
+            onClick={() => setShowPassword(!showPassword)}
+            className={classNames.button}
+            disabled={disabled}
+          >
+            {showPassword ? <InvisibilityOff /> : <Eye disabled={disabled} />}
           </button>
         )}
-        {isShowIconSearch && <Search />}
+        {isShowIconSearch && (
+          <button className={classNames.buttonSearch}>
+            <Search disabled={disabled} />
+          </button>
+        )}
       </div>
       {/*add Textarea component for error message*/}
-      {errorMessage && <div>{errorMessage}</div>}
+      {errorMessage && <div className={classNames.subField}>{errorMessage}</div>}
     </div>
   )
 }
