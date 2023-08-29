a54efb8566a02e6555d0fc85319d75dd538eab47
Ekaterina <ushakova.esu@gmail.com>
2023-08-29 22:32:10 +0600

    Button styles

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,20 @@ export function App() {
 
   return (
     <div>
-      <Button as={'button'}>Text</Button>
+      <Button as={'button'} variant="primary" disabled={true}>
+        Button Primary
+      </Button>
+      <div>
+        <Button as={'button'} variant="secondary" disabled={true}>
+          Button Secondary
+        </Button>
+      </div>
+      <div>
+        <Button as={'button'} variant="tertiary" disabled={true}>
+          Tertiary
+        </Button>
+      </div>
+
       <Checkbox checked={checked} onChange={setCheked} disabled={false} />
       <Checkbox label="Check-box" checked={checked} onChange={setCheked} disabled={true} />
       <Checkbox label="Check-box" checked={checked} onChange={setCheked} />
