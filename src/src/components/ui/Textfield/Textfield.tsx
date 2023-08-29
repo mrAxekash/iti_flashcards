488c0c1026ab1bbafe9aff3aa79ecb146652387a
Briws <brightwiths@gmail.com>
2023-08-29 19:32:59 +0400

    In progress. Textfield ref fix.

--- src/src/components/ui/Textfield/Textfield.tsx
+++ src/src/components/ui/Textfield/Textfield.tsx
@@ -1,4 +1,4 @@
-import { useState, ComponentPropsWithoutRef, ComponentProps } from 'react'
+import { ComponentProps, ComponentPropsWithoutRef, forwardRef, useState } from 'react'
 
 import { clsx } from 'clsx'
 
@@ -15,73 +15,81 @@ export type TextfieldPropsType = {
   labelProps?: ComponentProps<'label'>
   containerProps?: ComponentProps<'div'>
 } & ComponentPropsWithoutRef<'input'>
-export const Textfield = (props: TextfieldPropsType) => {
-  const {
-    label,
-    errorMessage,
-    type,
-    onChangeValue,
-    labelProps,
-    containerProps,
-    disabled,
-    ...rest
-  } = props
-  const [showPassword, setShowPassword] = useState(false)
 
-  const isShowButtonPasswordShowed = type === 'password'
-  const isShowIconSearch = type === 'search'
+export const Textfield = forwardRef<HTMLInputElement, TextfieldPropsType>(
+  (
+    {
+      errorMessage,
+      placeholder,
+      type,
+      containerProps,
+      labelProps,
+      label,
+      onChange,
+      disabled,
+      ...rest
+    },
+    ref
+  ) => {
+    const [showPassword, setShowPassword] = useState(false)
 
-  const finalType = getFinalType(type, showPassword)
+    const isShowButtonPasswordShowed = type === 'password'
+    const isShowIconSearch = type === 'search'
 
-  const classNames = {
-    container: clsx(textfieldStyle.container, containerProps?.className),
-    wrapper: clsx(textfieldStyle.inputWrapper),
-    button: clsx(
-      type === 'password' && textfieldStyle.button,
-      type === 'search' && textfieldStyle.buttonSearch
-    ),
-    inputField: clsx(
-      textfieldStyle.inputField,
-      type === 'search' && textfieldStyle.inputFieldSearch,
-      errorMessage && textfieldStyle.inputError
-    ),
-    label: clsx(textfieldStyle.label, disabled && textfieldStyle.disabled, labelProps?.className),
-    buttonSearch: clsx(textfieldStyle.buttonSearch),
-    subField: clsx(textfieldStyle.subField, textfieldStyle.error),
-  }
+    const finalType = getFinalType(type, showPassword)
 
-  return (
-    <div className={classNames.container}>
-      {/*Add Textarea component for label*/}
-      {label && <label className={classNames.label}> {label}</label>}
-      <div className={classNames.wrapper}>
-        <input
-          type={finalType}
-          className={classNames.inputField}
-          placeholder={'Hello'}
-          disabled={disabled}
-          {...rest}
-        />
-        {isShowButtonPasswordShowed && (
-          <button
-            onClick={() => setShowPassword(!showPassword)}
-            className={classNames.button}
+    const classNames = {
+      container: clsx(textfieldStyle.container, containerProps?.className),
+      wrapper: clsx(textfieldStyle.inputWrapper),
+      button: clsx(
+        type === 'password' && textfieldStyle.button,
+        type === 'search' && textfieldStyle.buttonSearch
+      ),
+      inputField: clsx(
+        textfieldStyle.inputField,
+        type === 'search' && textfieldStyle.inputFieldSearch,
+        errorMessage && textfieldStyle.inputError
+      ),
+      label: clsx(textfieldStyle.label, disabled && textfieldStyle.disabled, labelProps?.className),
+      buttonSearch: clsx(textfieldStyle.buttonSearch),
+      subField: clsx(textfieldStyle.subField, textfieldStyle.error),
+    }
+
+    return (
+      <div className={classNames.container}>
+        {/*Add Textarea component for label*/}
+        {label && <label className={classNames.label}> {label}</label>}
+        <div className={classNames.wrapper}>
+          <input
+            type={finalType}
+            className={classNames.inputField}
+            placeholder={'Hello'}
             disabled={disabled}
-          >
-            {showPassword ? <InvisibilityOff /> : <Eye disabled={disabled} />}
-          </button>
-        )}
-        {isShowIconSearch && (
-          <button className={classNames.buttonSearch}>
-            <Search disabled={disabled} />
-          </button>
-        )}
+            ref={ref}
+            {...rest}
+          />
+          {isShowButtonPasswordShowed && (
+            <button
+              onClick={() => setShowPassword(!showPassword)}
+              className={classNames.button}
+              disabled={disabled}
+              type={'button'}
+            >
+              {showPassword ? <InvisibilityOff /> : <Eye disabled={disabled} />}
+            </button>
+          )}
+          {isShowIconSearch && (
+            <button className={classNames.buttonSearch}>
+              <Search disabled={disabled} />
+            </button>
+          )}
+        </div>
+        {/*add Textarea component for error message*/}
+        {errorMessage && <div className={classNames.subField}>{errorMessage}</div>}
       </div>
-      {/*add Textarea component for error message*/}
-      {errorMessage && <div className={classNames.subField}>{errorMessage}</div>}
-    </div>
-  )
-}
+    )
+  }
+)
 
 const getFinalType = (type: ComponentProps<'input'>['type'], showPassword: boolean) => {
   if (type === 'password' && showPassword) {
@@ -90,3 +98,8 @@ const getFinalType = (type: ComponentProps<'input'>['type'], showPassword: boole
 
   return type
 }
+
+/*
+addons:
++added type button in button
+*/
