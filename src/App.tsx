628c281c2369ba8549cb12861a9e8ade2a8963af
Briws <brightwiths@gmail.com>
2023-09-04 21:25:52 +0400

    cleared App

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,3 @@
-import { SignInForm } from '@/components/auth/signIn-form/signIn-form'
-import { SignUpForm } from '@/components/auth/signUp-form/signUp-form.tsx'
-
 export function App() {
-  return (
-    <div>
-      <SignInForm />
-      <SignUpForm />
-    </div>
-  )
+  return <div></div>
 }
