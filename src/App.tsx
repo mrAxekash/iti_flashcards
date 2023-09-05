2cad1e2784efe300aa48c29b73f673f97249be2a
Briws <brightwiths@gmail.com>
2023-09-05 21:19:37 +0400

    cleared app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
-import { CheckEmailForm } from '@/components/auth/check-email/Check-email-form.tsx'
-
 export function App() {
   return (
     <div>
-      <CheckEmailForm userMail={'caramba-bob.com'} />
     </div>
   )
 }
