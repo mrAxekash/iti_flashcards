fad4f28a668f2408dcc5a5eeab7b37ea5a29770d
mrAxekash <aliaxandr_ku@mail.ru>
2023-09-06 23:01:14 +0300

    personal-information-form-2 part 3: add stories

--- src/components/personal-information/personal-information.tsx
+++ src/components/personal-information/personal-information.tsx
@@ -16,11 +16,17 @@ type PersonalInformationType = {
   userName?: string
   email?: string
   avatar?: string
+  onChangeUserName?: (userName: string) => void
+  onLogout?: () => void
+  onChangeAvatar?: (newAvatar: string) => void
 }
 export const PersonalInformation = ({
   userName = 'Ivan',
   email = 'google-shmoogle.gsh.com',
-  avatar,
+  avatar = 'https://img.freepik.com/free-vector/cute-cat-gaming-cartoon_138676-2969.jpg?w=826&t=st=1693944282~exp=1693944882~hmac=db975532c35e66aee49662f13349eb1ca1eab57963a6931222633c594a4f5a90',
+  onChangeUserName,
+  onChangeAvatar,
+  onLogout,
 }: PersonalInformationType) => {
   const classNames = {
     imageContainer: clsx(s.imageContainer),
@@ -38,8 +44,23 @@ export const PersonalInformation = ({
   }
 
   const [editMode, setEditMode] = useState(false)
-  const onClickHandler = () => {
+  const onChangeEditMode = () => {
+    onChangeAvatar && onChangeAvatar('newAvatar')
+    onChangeUserName && onChangeUserName('New Name')
+
     setEditMode(!editMode)
+  }
+
+  // const changeAvatarHandler = () => {
+  //   onChangeAvatar && onChangeAvatar('newAvatar')
+  // }
+  //
+  // const changeUserNameHandler = () => {
+  //   onChangeUserName && onChangeUserName('New Name')
+  // }
+
+  const logoutHandler = () => {
+    onLogout && onLogout()
   }
 
   return (
@@ -50,19 +71,22 @@ export const PersonalInformation = ({
       <div className={classNames.imageContainer}>
         <img src={avatar} alt="personalImg" className={classNames.image} />
         {!editMode && (
-          <Button variant={'secondary'} className={classNames.imageButton}>
+          <Button
+            variant={'secondary'}
+            className={classNames.imageButton}
+            onClick={onChangeEditMode}
+          >
             <Edit color={'var(--color-light-100)'} className={classNames.iconButton} />
           </Button>
         )}
       </div>
-      {/*///////////////////////////////////////////*/}
       {editMode ? (
         <div className={classNames.editModeContainer}>
           <Textfield label={'Nickname'} type={'text'} placeholder={userName} onChange={() => {}} />
           <Button
             variant={'primary'}
             fullWidth={true}
-            onClick={onClickHandler}
+            onClick={onChangeEditMode}
             className={classNames.editModeButton}
           >
             Save Changes
@@ -74,7 +98,7 @@ export const PersonalInformation = ({
             <Typography variant={'H1'} className={classNames.userName}>
               {userName}
             </Typography>
-            <Button variant={'link'} onClick={onClickHandler}>
+            <Button variant={'link'} onClick={onChangeEditMode}>
               <Edit color={'var(--color-light-100)'} className={classNames.iconButton} />
             </Button>
           </div>
@@ -84,14 +108,12 @@ export const PersonalInformation = ({
             </Typography>
           </div>
 
-          <Button variant={'secondary'} className={classNames.logButton}>
+          <Button variant={'secondary'} className={classNames.logButton} onClick={logoutHandler}>
             <Logout color={'var(--color-light-100)'} />
             Logout
           </Button>
         </>
       )}
-
-      {/*////////*/}
     </Card>
   )
 }
