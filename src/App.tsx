4f7b93ea28a4920a90649a4557c1da74f8e18cd1
Briws <brightwiths@gmail.com>
2023-09-07 17:49:57 +0400

    made RadioGroup controlled

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import { RadioGroupDemo } from '@/src/components/ui/RadioGroup/RadioGroup.tsx'
+import {RadioElementType, RadioGroupDemo} from '@/src/components/ui/RadioGroup/RadioGroup.tsx'
+import {useState} from "react"
 
 export function App() {
+
+  const arr: RadioElementType[] = [
+    { id: 'r1', value: 'default', label: 'Default' },
+    { id: 'r2', value: 'comfortable', label: 'Comfortable' },
+    { id: 'r3', value: 'compact', label: 'Compact' },
+  ]
+  const [value, setValue] = useState(arr[0].value)
+
   return (
     <div>
-      <RadioGroupDemo />
+      <RadioGroupDemo options={arr} onValueChange={setValue} value={value}/>
     </div>
   )
 }
--- src/src/components/ui/RadioGroup/RadioGroup.tsx
+++ src/src/components/ui/RadioGroup/RadioGroup.tsx
@@ -1,17 +1,9 @@
-import { useState } from 'react'
-
 import * as RadioGroup from '@radix-ui/react-radio-group'
 import './styles.css'
 
-export const RadioGroupDemo = () => {
-  const [value, setValue] = useState()
-  const arr: ElementType[] = [
-    { id: 'r1', value: 'default', label: 'Default' },
-    { id: 'r2', value: 'comfortable', label: 'Comfortable' },
-    { id: 'r3', value: 'compact', label: 'Compact' },
-  ]
+export const RadioGroupDemo = (props: PropsType) => {
 
-  const Element = (props: ElementType) => {
+  const Element = (props: RadioElementType) => {
     return (
       <div style={{ display: 'flex', alignItems: 'center' }}>
         <RadioGroup.Item className="RadioGroupItem" value={props.value} id={props.id}>
@@ -27,19 +19,26 @@ export const RadioGroupDemo = () => {
   return (
     <RadioGroup.Root
       className="RadioGroupRoot"
-      defaultValue={arr[0].value}
+      defaultValue={props.value}
       aria-label="View density"
-      value={value}
+      value={props.value}
+      onValueChange={(e) => {props.onValueChange(e)}}
     >
-      {arr.map(e => (
+      {props.options.map(e => (
         <Element key={e.id} id={e.id} value={e.value} label={e.label} />
       ))}
     </RadioGroup.Root>
   )
 }
 
-type ElementType = {
+export type RadioElementType = {
   id: string
   value: string
   label: string
+}
+
+type PropsType = {
+  value: string
+  onValueChange: (e: string) => void
+  options: Array<RadioElementType>
 }
