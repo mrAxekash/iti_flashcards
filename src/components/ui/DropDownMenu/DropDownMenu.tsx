a9d2a0ab9e4356dfe84312bcb7f1ffe72c139850
mrAxekash <aliaxandr_ku@mail.ru>
2023-09-09 00:34:11 +0300

    #dropDownMenu: part 4: merge with branch drapdownMenu & add styles

--- src/components/ui/DropDownMenu/DropDownMenu.tsx
+++ src/components/ui/DropDownMenu/DropDownMenu.tsx
@@ -2,6 +2,8 @@ import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
 
 import s from './DropDownMenu.module.scss'
 
+import { Logout } from '@/assets/icons/Logout.tsx'
+import { Person } from '@/assets/icons/Person.tsx'
 import { Typography } from '@/components/ui/Typography'
 
 type DropDownMenuType = {
@@ -11,55 +13,44 @@ type DropDownMenuType = {
 }
 export const DropDownMenu = ({ avatar, userName, userEmail }: DropDownMenuType) => {
   return (
-    <div style={{ marginLeft: '100px', marginTop: '30px' }}>
+    <div style={{ marginLeft: '200px', marginTop: '30px' }}>
       <DropdownMenu.Root>
         <DropdownMenu.Trigger asChild>
-          {/*<button className={s.button}>*/}
-          <img src={avatar} alt="avatar" className={s.userAvatar} />
-          {/*</button>*/}
+          <button className={s.button}>
+            <img src={avatar} alt="avatar" className={s.userAvatar} />
+          </button>
         </DropdownMenu.Trigger>
 
         <DropdownMenu.Portal>
-          <DropdownMenu.Content className={s.dropDownMenuContent}>
-            <DropdownMenu.Arrow width={10} height={8} className={s.dropDownMenuArrow} />
+          <DropdownMenu.Content
+            avoidCollisions={true}
+            align={'end'}
+            className={s.dropDownMenuContent}
+          >
+            <DropdownMenu.Arrow width={15} height={8} className={s.dropDownMenuArrow} />
 
             <DropdownMenu.Item className={s.dropDownMenuItem}>
               <img src={avatar} alt="avatar" className={s.userAvatar} />
               <div>
-                <Typography variant={'Subtitle_2'}> {userName} </Typography>
-                <Typography variant={'Caption'}>{userEmail}</Typography>
+                <Typography variant={'Subtitle_2'} className={s.userName}>
+                  {' '}
+                  {userName}{' '}
+                </Typography>
+                <Typography variant={'Caption'} className={s.userEmail}>
+                  {userEmail}
+                </Typography>
               </div>
             </DropdownMenu.Item>
-            <DropdownMenu.Item>
+            <DropdownMenu.Separator className={s.dropDownMenuSeparator} />
+            <DropdownMenu.Item className={s.dropDownMenuItem}>
+              <Person color={'var(--color-light-100)'} className={s.icons} />
               <Typography variant={'Caption'}>My Profile</Typography>{' '}
             </DropdownMenu.Item>
-            <DropdownMenu.Item>
+            <DropdownMenu.Separator className={s.dropDownMenuSeparator} />
+            <DropdownMenu.Item className={s.dropDownMenuItem}>
+              <Logout color={'var(--color-light-100)'} className={s.icons} />
               <Typography variant={'Caption'}>Sign Out</Typography>{' '}
             </DropdownMenu.Item>
-
-            <DropdownMenu.Group>
-              <DropdownMenu.Item />
-            </DropdownMenu.Group>
-
-            <DropdownMenu.CheckboxItem>
-              <DropdownMenu.ItemIndicator />
-            </DropdownMenu.CheckboxItem>
-
-            <DropdownMenu.RadioGroup>
-              {/*<DropdownMenu.RadioItem>*/}
-              {/*  <DropdownMenu.ItemIndicator />*/}
-              {/*</DropdownMenu.RadioItem>*/}
-            </DropdownMenu.RadioGroup>
-
-            <DropdownMenu.Sub>
-              <DropdownMenu.SubTrigger />
-              <DropdownMenu.Portal>
-                <DropdownMenu.SubContent />
-              </DropdownMenu.Portal>
-            </DropdownMenu.Sub>
-
-            <DropdownMenu.Separator />
-            <DropdownMenu.Arrow />
           </DropdownMenu.Content>
         </DropdownMenu.Portal>
       </DropdownMenu.Root>
