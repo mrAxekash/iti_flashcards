c881bf3c073bff6f802855671d86c5a3d3150850
Ekaterina <ushakova.esu@gmail.com>
2023-09-15 20:36:34 +0600

    changing schema, button, puls vector

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,11 @@
-import { useState } from 'react'
-
-import { Slider } from '@/components/ui/Slider/slider.tsx'
+import { Header } from '@/components/ui/Header/header.tsx'
 
 export function App() {
-  const [value, setValue] = useState([1, 12])
+  //const [value, setValue] = useState([1, 12])
 
   return (
     <div>
-      <Slider
-        defaultValue={[1]}
-        value={value}
-        onValueChange={values => setValue(values)}
-        step={1}
-        min={1}
-        max={12}
-        minStepsBetweenThumbs={1}
-      />
+      <Header />
     </div>
   )
 }
--- src/components/ui/Header/header.tsx
+++ src/components/ui/Header/header.tsx
@@ -5,6 +5,7 @@ import { clsx } from 'clsx'
 import s from './header.module.scss'
 
 import { Button } from '@/components/ui/Button'
+import { Vector } from '@/components/ui/Button/vector.tsx'
 import Logo from '@/components/ui/Header/logo.tsx'
 
 export type HeaderProps = ComponentProps<'header'>
@@ -24,6 +25,7 @@ export const Header: FC<HeaderProps> = ({ className, ...rest }) => {
             <div className={s.button}>avatar</div>
           ) : (
             <Button className={s.button} variant="primary">
+              <Vector />
               Sign In
             </Button>
           )}
