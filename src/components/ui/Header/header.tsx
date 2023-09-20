e1c0cdc518ea695e297feb230eaf601b54fe2d34
Briws <brightwiths@gmail.com>
2023-09-21 02:29:33 +0400

    Progress with adding DropdownMenu

--- src/components/ui/Header/header.tsx
+++ src/components/ui/Header/header.tsx
@@ -1,17 +1,20 @@
 import {ComponentProps, FC, useEffect, useState} from 'react'
-
 import {clsx} from 'clsx'
-
 import s from './header.module.scss'
+import defaultAva from '@/assets/images/defaultAva.png'
 
 import {Button} from '@/components/ui/Button'
 import {Vector} from '@/components/ui/Button/vector.tsx'
 import Logo from '@/components/ui/Header/logo.tsx'
 import {useGetMeQuery} from "@/services/auth/auth.service.ts"
+import {DropdownItemWithAvatar} from "@/components/ui/DropDownMenu/DropdownMenuWithAvatar/DropdownMenuWithAvatar"
+import {Avatar} from "@/components/ui/Avatar/Avatar.tsx"
+import {Typography} from "@/components/ui/Typography"
+import {DropDownMenu} from "@/components/ui/DropDownMenu/DropDownMenu.tsx"
 
 export type HeaderProps = ComponentProps<'header'>
 
-export const Header: FC<HeaderProps> = ({ className, ...rest }) => {
+export const Header: FC<HeaderProps> = ({className, ...rest}) => {
   const {data: me} = useGetMeQuery()
 
   const [isLoggedIn, setIsLoggedIn] = useState(false)
@@ -29,15 +32,24 @@ export const Header: FC<HeaderProps> = ({ className, ...rest }) => {
     <>
       <header className={classNames.header} {...rest}>
         <div className={s.wrapper}>
-          <Logo />
+          <Logo/>
           {isLoggedIn ? (
             <div className={s.containerNameAva}>
               <div className={s.name}>{me && me.name}</div>
-              <div className={s.button}>avatar</div>
+              <DropDownMenu trigger={<Avatar urlAdress={defaultAva}/>}>
+                <DropdownItemWithAvatar trigger={<Avatar urlAdress={defaultAva}/>}>
+                  <Typography variant={'Subtitle_2'} className={s.userName}>
+                    {me && me.name}
+                  </Typography>
+                  <Typography variant={'Caption'} className={s.userEmail}>
+                    {me && me.email}
+                  </Typography>
+                </DropdownItemWithAvatar>
+              </DropDownMenu>
             </div>
           ) : (
             <Button className={s.button} variant="primary">
-              <Vector />
+              <Vector/>
               Sign In
             </Button>
           )}
