b98c847ddcd271cefb492f4854a7333391ead20b
Briws <brightwiths@gmail.com>
2023-09-20 04:03:49 +0400

    condition for header

--- src/components/ui/Header/header.tsx
+++ src/components/ui/Header/header.tsx
@@ -1,27 +1,36 @@
-import { ComponentProps, FC } from 'react'
+import {ComponentProps, FC, useEffect, useState} from 'react'
 
-import { clsx } from 'clsx'
+import {clsx} from 'clsx'
 
 import s from './header.module.scss'
 
-import { Button } from '@/components/ui/Button'
-import { Vector } from '@/components/ui/Button/vector.tsx'
+import {Button} from '@/components/ui/Button'
+import {Vector} from '@/components/ui/Button/vector.tsx'
 import Logo from '@/components/ui/Header/logo.tsx'
+import {useGetMeQuery} from "@/services/auth/auth.service.ts"
 
 export type HeaderProps = ComponentProps<'header'>
 
 export const Header: FC<HeaderProps> = ({ className, ...rest }) => {
+  const {data: me} = useGetMeQuery()
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false)
+
+  useEffect(() => {
+    if (me && me?.success === false) setIsLoggedIn(false)
+    else setIsLoggedIn(true)
+  }, [me])
+
   const classNames = {
     header: clsx(s.header, className),
   }
-  const isLogied = false
 
   return (
     <>
       <header className={classNames.header} {...rest}>
         <div className={s.wrapper}>
           <Logo />
-          {isLogied ? (
+          {isLoggedIn ? (
             <div className={s.button}>avatar</div>
           ) : (
             <Button className={s.button} variant="primary">
