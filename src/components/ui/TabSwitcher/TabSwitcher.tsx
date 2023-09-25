73b9c17bfdc4422664acf01273d5c8217719d26e
Briws <brightwiths@gmail.com>
2023-09-26 03:09:37 +0400

    realization with onClick

--- src/components/ui/TabSwitcher/TabSwitcher.tsx
+++ src/components/ui/TabSwitcher/TabSwitcher.tsx
@@ -1,62 +1,29 @@
+import { useEffect, useState } from 'react'
+
 import * as Tabs from '@radix-ui/react-tabs'
 
 import s from './TabSwitcher.module.scss'
 
-import './styles.css'
-
 export const TabSwitcher = () => {
+  const [activeTabIndex, setActiveTabIndex] = useState(1)
+
+  useEffect(() => {
+    console.log(`Selected tab: ${activeTabIndex}`)
+  }, [activeTabIndex])
+
   return (
     <Tabs.Root className={s.TabsRoot} defaultValue="tab1">
       <Tabs.List className={s.TabsList} aria-label="Manage your account">
-        <Tabs.Trigger className={s.TabsTrigger} value="tab1">
-          Account
+        <Tabs.Trigger className={s.TabsTrigger} value="tab1" onClick={() => setActiveTabIndex(1)}>
+          Switcher 1
         </Tabs.Trigger>
-        <Tabs.Trigger className={s.TabsTrigger} value="tab2">
-          Password
+        <Tabs.Trigger className={s.TabsTrigger} value="tab2" onClick={() => setActiveTabIndex(2)}>
+          Switcher 2
+        </Tabs.Trigger>
+        <Tabs.Trigger className={s.TabsTrigger} value="tab3" onClick={() => setActiveTabIndex(3)}>
+          Switcher 3
         </Tabs.Trigger>
       </Tabs.List>
-      <Tabs.Content className={s.TabsContent} value="tab1">
-        <p className={s.Text}>Make changes to your account here. Click save when you're done.</p>
-        <fieldset className={s.Fieldset}>
-          <label className={s.Label} htmlFor="name">
-            Name
-          </label>
-          <input className={s.Input} id="name" defaultValue="Pedro Duarte" />
-        </fieldset>
-        <fieldset className={s.Fieldset}>
-          <label className={s.Label} htmlFor="username">
-            Username
-          </label>
-          <input className={s.Input} id="username" defaultValue="@peduarte" />
-        </fieldset>
-        <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
-          <button className={s.ButtonGreen}>Save changes</button>
-        </div>
-      </Tabs.Content>
-      <Tabs.Content className={s.TabsContent} value="tab2">
-        <p className={s.Text}>Change your password here. After saving, you'll be logged out.</p>
-        <fieldset className={s.Fieldset}>
-          <label className={s.Label} htmlFor="currentPassword">
-            Current password
-          </label>
-          <input className={s.Input} id="currentPassword" type="password" />
-        </fieldset>
-        <fieldset className={s.Fieldset}>
-          <label className={s.Label} htmlFor="newPassword">
-            New password
-          </label>
-          <input className={s.Input} id="newPassword" type="password" />
-        </fieldset>
-        <fieldset className={s.Fieldset}>
-          <label className={s.Label} htmlFor="confirmPassword">
-            Confirm password
-          </label>
-          <input className={s.Input} id="confirmPassword" type="password" />
-        </fieldset>
-        <div style={{ display: 'flex', marginTop: 20, justifyContent: 'flex-end' }}>
-          <button className={s.ButtonGreen}>Change password</button>
-        </div>
-      </Tabs.Content>
     </Tabs.Root>
   )
 }
