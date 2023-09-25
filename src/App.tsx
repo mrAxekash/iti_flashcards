f726ad1e744fbda1500a1e6dd460e1227a0f1f06
Briws <brightwiths@gmail.com>
2023-09-26 01:44:11 +0400

    begin of TabSwitcher

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
-import { Provider } from 'react-redux'
-
 import s from './App.module.scss'
 
-import { Header } from '@/components/ui/Header/header.tsx'
-import { Router } from '@/router.tsx'
-import { store } from '@/services/store.ts'
+import { TabSwitcher } from '@/components/ui/TabSwitcher/TabSwitcher.tsx'
 
 export function App() {
   return (
     <div className={s.app}>
-      <Provider store={store}>
-        <Header />
-        <div className={s.container}>
-          <Router />
-        </div>
-      </Provider>
+      <div className={s.container}>
+        <TabSwitcher />
+      </div>
     </div>
   )
 }
