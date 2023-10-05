20cada8a33662949b477bcc9208370a30550c76c
Ekaterina <ushakova.esu@gmail.com>
2023-10-05 23:54:26 +0600

    Check-email-page

--- src/pages/check-email-page/check-email-page.tsx
+++ src/pages/check-email-page/check-email-page.tsx
@@ -1,12 +1,9 @@
-import { Navigate } from 'react-router-dom'
-
 import { CheckEmailForm } from '@/components/auth/check-email/Check-email-form.tsx'
-import { useCheckEmailMutation } from '@/services/auth/auth.service.ts'
 
 export const CheckEmailPage = () => {
-  const [checkEmail, { isSuccess }] = useCheckEmailMutation()
+  /*const [checkEmail, { isSuccess }] = useCheckEmailMutation()
 
-  if (isSuccess) return <Navigate to="/login" />
+  if (isSuccess) return <Navigate to="/login" />*/
 
   return (
     <div>
