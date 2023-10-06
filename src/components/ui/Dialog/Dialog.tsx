439eb09e219204168debd78f85b5d2b5a91dba3f
Briws <brightwiths@gmail.com>
2023-10-06 22:16:50 +0400

    module classes

--- src/components/ui/Dialog/Dialog.tsx
+++ src/components/ui/Dialog/Dialog.tsx
@@ -2,8 +2,9 @@ import { useState } from 'react'
 
 import * as Dialog from '@radix-ui/react-dialog'
 import { Cross2Icon } from '@radix-ui/react-icons'
+import { clsx } from 'clsx'
 
-import './styles.css'
+import s from './Dialog.module.scss'
 
 const DialogDemo = () => {
   const [open, setOpen] = useState(true)
@@ -13,34 +14,34 @@ const DialogDemo = () => {
   return (
     <Dialog.Root open={open} onOpenChange={setOpen}>
       <Dialog.Trigger asChild>
-        <button className="Button violet">Edit profile</button>
+        <button className={clsx(s.Button, s.ButtonViolet)}>Edit profile</button>
       </Dialog.Trigger>
       <Dialog.Portal>
-        <Dialog.Overlay className="DialogOverlay" />
-        <Dialog.Content className="DialogContent">
-          <Dialog.Title className="DialogTitle">Edit profile</Dialog.Title>
-          <Dialog.Description className="DialogDescription">
-            Make changes to your profile here. Click save when you're done.
+        <Dialog.Overlay className={s.DialogOverlay} />
+        <Dialog.Content className={s.DialogContent}>
+          <Dialog.Title className={s.DialogTitle}>Edit profile</Dialog.Title>
+          <Dialog.Description className={s.DialogDescription}>
+            Make changes to your profile here. Click save when you&#39;re done.
           </Dialog.Description>
-          <fieldset className="Fieldset">
-            <label className="Label" htmlFor="name">
+          <fieldset className={s.Fieldset}>
+            <label className={s.Label} htmlFor="name">
               Name
             </label>
-            <input className="Input" id="name" defaultValue="Pedro Duarte" />
+            <input className={s.Input} id="name" defaultValue="Pedro Duarte" />
           </fieldset>
-          <fieldset className="Fieldset">
-            <label className="Label" htmlFor="username">
+          <fieldset className={s.Fieldset}>
+            <label className={s.Label} htmlFor="username">
               Username
             </label>
-            <input className="Input" id="username" defaultValue="@peduarte" />
+            <input className={s.Input} id="username" defaultValue="@peduarte" />
           </fieldset>
           <div style={{ display: 'flex', marginTop: 25, justifyContent: 'flex-end' }}>
             <Dialog.Close asChild>
-              <button className="Button green">Save changes</button>
+              <button className={clsx(s.Button, s.ButtonGreen)}>Save changes</button>
             </Dialog.Close>
           </div>
           <Dialog.Close asChild>
-            <button className="IconButton" aria-label="Close">
+            <button className={s.IconButton} aria-label="Close">
               <Cross2Icon />
             </button>
           </Dialog.Close>
