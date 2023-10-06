697c870df375443455283ede743a41eb3699172b
Briws <brightwiths@gmail.com>
2023-10-06 23:59:35 +0400

    dialog progress

--- src/components/ui/Dialog/Dialog.tsx
+++ src/components/ui/Dialog/Dialog.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 
 import * as Dialog from '@radix-ui/react-dialog'
-import { Cross2Icon } from '@radix-ui/react-icons'
 import { clsx } from 'clsx'
 
 import s from './Dialog.module.scss'
+
+import closeIcon from '@/assets/icons/close.png'
+import { Button } from '@/components/ui/Button'
 
 const DialogDemo = () => {
   const [open, setOpen] = useState(true)
@@ -19,32 +21,25 @@ const DialogDemo = () => {
       <Dialog.Portal>
         <Dialog.Overlay className={s.DialogOverlay} />
         <Dialog.Content className={s.DialogContent}>
-          <Dialog.Title className={s.DialogTitle}>Edit profile</Dialog.Title>
+          <Dialog.Title className={s.DialogTitle}>
+            <div>Delete Pack</div>
+            <Dialog.Close asChild>
+              <button className={s.IconButton} aria-label="Close">
+                <img src={closeIcon} alt="closeIcon" />
+              </button>
+            </Dialog.Close>
+          </Dialog.Title>
           <Dialog.Description className={s.DialogDescription}>
-            Make changes to your profile here. Click save when you&#39;re done.
+            Do you really want to remove Pack Name?
+            <br />
+            All cards will be deleted.
           </Dialog.Description>
-          <fieldset className={s.Fieldset}>
-            <label className={s.Label} htmlFor="name">
-              Name
-            </label>
-            <input className={s.Input} id="name" defaultValue="Pedro Duarte" />
-          </fieldset>
-          <fieldset className={s.Fieldset}>
-            <label className={s.Label} htmlFor="username">
-              Username
-            </label>
-            <input className={s.Input} id="username" defaultValue="@peduarte" />
-          </fieldset>
           <div style={{ display: 'flex', marginTop: 25, justifyContent: 'flex-end' }}>
             <Dialog.Close asChild>
-              <button className={clsx(s.Button, s.ButtonGreen)}>Save changes</button>
+              <Button className={clsx(s.Button, s.ButtonGreen)}>Cancel</Button>
             </Dialog.Close>
+            <Button className={clsx(s.Button, s.ButtonGreen)}>Delete pack</Button>
           </div>
-          <Dialog.Close asChild>
-            <button className={s.IconButton} aria-label="Close">
-              <Cross2Icon />
-            </button>
-          </Dialog.Close>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog.Root>
