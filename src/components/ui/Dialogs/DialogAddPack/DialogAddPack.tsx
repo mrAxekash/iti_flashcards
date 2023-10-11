7db37785fe3f9bb64f1dbfdd6f6422cfc37c7a07
Briws <brightwiths@gmail.com>
2023-10-12 00:48:53 +0400

    part of DialogRemovePack separated to DialogsCommon

--- src/components/ui/Dialogs/DialogAddPack/DialogAddPack.tsx
+++ src/components/ui/Dialogs/DialogAddPack/DialogAddPack.tsx
@@ -1,54 +1,40 @@
-import * as RDialog from '@radix-ui/react-dialog'
+import { Dispatch, SetStateAction } from 'react'
 
-import closeIcon from '@/assets/icons/close.png'
-import { Button } from '@/components/ui/Button'
 import { Checkbox } from '@/components/ui/Checkbox'
 import s from '@/components/ui/Dialogs/DialogsCommon.module.scss'
+import { DialogsCommon } from '@/components/ui/Dialogs/DialogsCommon.tsx'
 import { Textfield } from '@/components/ui/Textfield'
 
 export const DialogAddPack = (props: PropsType) => {
   return (
-    <RDialog.Root open={props.open} onOpenChange={props.setOpen}>
-      <RDialog.Portal>
-        <RDialog.Overlay className={s.DialogOverlay} />
-        <RDialog.Content className={s.DialogContent}>
-          <RDialog.Title className={s.DialogTitle}>
-            <div>Add New Pack</div>
-            <RDialog.Close asChild>
-              <button className={s.IconButton} aria-label="Close">
-                <img src={closeIcon} alt="closeIcon" />
-              </button>
-            </RDialog.Close>
-          </RDialog.Title>
-          <div className={s.DialogDescription}>
-            <div className={s.textFieldContainer}>
-              <Textfield
-                label={'Name Pack'}
-                onChange={e => props.onChangeNewPackName(e.currentTarget.value)}
-                value={props.newPackName}
-              />
-            </div>
-            <Checkbox
-              label={'Private pack'}
-              checked={props.isPrivate}
-              onValueChange={props.setIsPrivate}
-            />
-          </div>
-          <div className={s.buttonContainer}>
-            <RDialog.Close asChild>
-              <Button className={s.buttonCancel}>Cancel</Button>
-            </RDialog.Close>
-            <Button onClick={props.onAdd}>Add pack</Button>
-          </div>
-        </RDialog.Content>
-      </RDialog.Portal>
-    </RDialog.Root>
+    <DialogsCommon
+      title={'Add New Pack'}
+      open={props.open}
+      setOpen={props.setOpen}
+      onButtonAction={props.onAdd}
+      actionButtonText={'Add New Pack'}
+    >
+      <div className={s.DialogDescription}>
+        <div className={s.textFieldContainer}>
+          <Textfield
+            label={'Name Pack'}
+            onChange={e => props.onChangeNewPackName(e.currentTarget.value)}
+            value={props.newPackName}
+          />
+        </div>
+        <Checkbox
+          label={'Private pack'}
+          checked={props.isPrivate}
+          onValueChange={props.setIsPrivate}
+        />
+      </div>
+    </DialogsCommon>
   )
 }
 
 type PropsType = {
   open: boolean
-  setOpen: (open: boolean) => void
+  setOpen: Dispatch<SetStateAction<boolean>>
   newPackName: string
   onAdd: () => void
   onChangeNewPackName: (newPackName: string) => void
