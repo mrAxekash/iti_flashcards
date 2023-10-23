a51743f93188496d27aad89fca406e68149a6d6c
Briws <brightwiths@gmail.com>
2023-10-24 00:56:44 +0400

    card remove: moved, deleted types

--- src/services/cards.service.ts
+++ src/services/cards.service.ts
@@ -1,7 +1,7 @@
 import { baseApi } from '@/services/base-api.ts'
 const cardsService = baseApi.injectEndpoints({
   endpoints: builder => ({
-    deleteCard: builder.mutation<any, { id: string }>({
+    deleteCard: builder.mutation<{}, { id: string }>({
       query: data => ({
         url: `/v1/cards/${data.id}`,
         method: 'DELETE',
