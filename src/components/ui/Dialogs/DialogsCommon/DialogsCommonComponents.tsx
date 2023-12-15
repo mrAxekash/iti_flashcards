3e2f204ec9db74a9fead8d7e1fb719fccd8ff792
Briws <brightwiths@gmail.com>
2023-12-15 22:34:53 +0700

    styling

--- src/components/ui/Dialogs/DialogsCommon/DialogsCommonComponents.tsx
+++ src/components/ui/Dialogs/DialogsCommon/DialogsCommonComponents.tsx
@@ -65,7 +65,7 @@ export const ComboChangeCoverDummyImgCropper = (props: ComboChangeCoverDummyImgC
                    value={props.slider.sliderValue[0]}
                    onValueChange={props.slider.sliderChangeHandler}
                  />
-                 <Button onClick={props.onApprove}>Approve</Button>
+                 <Button onClick={props.onApprove} variant="secondary" className={s.button}>Approve</Button>
                </>
                : <div className={sC.imgContainer}><img className={s.croppedImg} src={props.file.cropImg} alt="cropImg"/></div>
            }
