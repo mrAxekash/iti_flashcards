0409381c7f761819e367aa7f33b91974f09de78c
Briws <brightwiths@gmail.com>
2023-12-18 22:15:47 +0700

    fixed slider styles

--- src/components/ui/Slider/slider.tsx
+++ src/components/ui/Slider/slider.tsx
@@ -10,14 +10,18 @@ export const Slider = forwardRef<
   ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
 >(({ className, name, max, ...props }, ref) => (
   <div className={s.container}>
-    <span className={s.value}>{props?.value?.[0]}</span>
-    <SliderPrimitive.Root ref={ref} className={clsx(s.root, className)} {...props}>
+      <div>
+          <span className={s.value}>{props?.value?.[0]}</span>
+      </div>
+      <SliderPrimitive.Root ref={ref} className={clsx(s.root, className)} {...props}>
       <SliderPrimitive.Track className={s.track}>
         <SliderPrimitive.Range className={s.range} />
       </SliderPrimitive.Track>
       <SliderPrimitive.Thumb className={s.thumb} />
       <SliderPrimitive.Thumb className={s.thumb} />
     </SliderPrimitive.Root>
-    <span className={s.value}>{max}</span>
+      <div>
+          <span className={s.value}>{max}</span>
+      </div>
   </div>
 ))
