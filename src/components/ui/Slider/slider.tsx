f0890e4be2226913b56827efd36cde72959571ac
mrAxekash <aliaxandr_ku@mail.ru>
2023-12-19 23:19:47 +0300

    refactor: change slider part 2 fixed slider props

--- src/components/ui/Slider/slider.tsx
+++ src/components/ui/Slider/slider.tsx
@@ -10,27 +10,29 @@ import { Typography } from '@/components/ui/Typography'
 export const Slider = forwardRef<
   ElementRef<typeof SliderPrimitive.Root>,
   ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
->(({ className, name, max, title, ...props }, ref) => (
-  <div>
-    {title && (
-      <Typography variant={'Body_2'} as={'label'}>
-        {title}
-      </Typography>
-    )}
-    <div className={s.container}>
-      <div>
-        <span className={s.value}>{props?.value?.[0]}</span>
-      </div>
-      <SliderPrimitive.Root ref={ref} className={clsx(s.root, className)} {...props}>
-        <SliderPrimitive.Track className={s.track}>
-          <SliderPrimitive.Range className={s.range} />`
-        </SliderPrimitive.Track>
-        <SliderPrimitive.Thumb className={s.thumb} />
-        <SliderPrimitive.Thumb className={s.thumb} />
-      </SliderPrimitive.Root>
-      <div>
-        <span className={s.value}>{props?.value?.[1]}</span>
+>(({ className, name, title, ...props }, ref) => {
+  return (
+    <div>
+      {title && (
+        <Typography variant={'Body_2'} as={'label'}>
+          {title}
+        </Typography>
+      )}
+      <div className={s.container}>
+        <div>
+          <span className={s.value}>{props?.value?.[0]}</span>
+        </div>
+        <SliderPrimitive.Root ref={ref} className={clsx(s.root, className)} {...props}>
+          <SliderPrimitive.Track className={s.track}>
+            <SliderPrimitive.Range className={s.range} />
+          </SliderPrimitive.Track>
+          <SliderPrimitive.Thumb className={s.thumb} />
+          <SliderPrimitive.Thumb className={s.thumb} />
+        </SliderPrimitive.Root>
+        <div>
+          <span className={s.value}>{props?.value?.[1]}</span>
+        </div>
       </div>
     </div>
-  </div>
-))
+  )
+})
