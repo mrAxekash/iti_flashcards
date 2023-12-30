9ed810befb1fa3572dc79b6631fe0e8b5302624d
Briws <brightwiths@gmail.com>
2023-12-30 21:36:21 +0700

    Fixed SignUp story

--- src/components/auth/signUp-form/SignUp-form.stories.tsx
+++ src/components/auth/signUp-form/SignUp-form.stories.tsx
@@ -1,20 +1,23 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type {Meta, StoryObj} from '@storybook/react'
 
-import { SignUpForm } from './signUp-form.tsx'
+import {SignUpForm} from './signUp-form.tsx'
 import {Provider} from "react-redux"
 import {store} from "@/services/store.ts"
+import {BrowserRouter} from "react-router-dom"
 
 const meta = {
-  title: 'Auth/SignUpForm',
-  component: SignUpForm,
-  tags: ['autodocs'],
-  decorators: [
-    (Story) => (
-        <Provider store={store}>
-          <Story />
-        </Provider>
-    ),
-  ],
+    title: 'Auth/SignUpForm',
+    component: SignUpForm,
+    tags: ['autodocs'],
+    decorators: [
+        (Story) => (
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Story/>
+                </BrowserRouter>
+            </Provider>
+        ),
+    ],
 } satisfies Meta<typeof SignUpForm>
 
 export default meta
