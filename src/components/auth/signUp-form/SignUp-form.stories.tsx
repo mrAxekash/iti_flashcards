9b1b7b83686cb46520fa9960314ff284a6c7828b
Briws <brightwiths@gmail.com>
2023-12-30 21:13:02 +0700

    Wrapped SignUp Story with Provider

--- src/components/auth/signUp-form/SignUp-form.stories.tsx
+++ src/components/auth/signUp-form/SignUp-form.stories.tsx
@@ -1,11 +1,20 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { SignUpForm } from './signUp-form.tsx'
+import {Provider} from "react-redux"
+import {store} from "@/services/store.ts"
 
 const meta = {
   title: 'Auth/SignUpForm',
   component: SignUpForm,
   tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+        <Provider store={store}>
+          <Story />
+        </Provider>
+    ),
+  ],
 } satisfies Meta<typeof SignUpForm>
 
 export default meta
