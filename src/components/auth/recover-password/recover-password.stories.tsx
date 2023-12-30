96f12c420813ea8c563d46741c10a6c15afe823d
Briws <brightwiths@gmail.com>
2023-12-30 21:39:01 +0700

    Fixed recover-password & SignIn-form stories

--- src/components/auth/recover-password/recover-password.stories.tsx
+++ src/components/auth/recover-password/recover-password.stories.tsx
@@ -1,18 +1,26 @@
-import type { Meta, StoryObj } from '@storybook/react'
+import type {Meta, StoryObj} from '@storybook/react'
 
-import { RecoverPassword } from '@/components/auth/recover-password/recover-password.tsx'
+import {RecoverPassword} from '@/components/auth/recover-password/recover-password.tsx'
+import {BrowserRouter} from "react-router-dom"
 
 const meta = {
-  title: 'Auth/Recover password',
-  component: RecoverPassword,
-  tags: ['autodocs'],
+    title: 'Auth/Recover password',
+    component: RecoverPassword,
+    tags: ['autodocs'],
+    decorators: [
+        (Story) => (
+            <BrowserRouter>
+                <Story/>
+            </BrowserRouter>
+        ),
+    ],
 } satisfies Meta<typeof RecoverPassword>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 export const Default: Story = {
-  args: {
-    onSubmit: (data: any) => console.info(data),
-  },
+    args: {
+        onSubmit: (data: any) => console.info(data),
+    },
 }
--- src/components/auth/signIn-form/SignIn-form.stories.tsx
+++ src/components/auth/signIn-form/SignIn-form.stories.tsx
@@ -1,11 +1,19 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
 import { SignIn } from './sign-in.tsx'
+import {BrowserRouter} from "react-router-dom"
 
 const meta = {
   title: 'Auth/SignInForm',
   component: SignIn,
   tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+        <BrowserRouter>
+          <Story/>
+        </BrowserRouter>
+    ),
+  ],
 } satisfies Meta<typeof SignIn>
 
 export default meta
