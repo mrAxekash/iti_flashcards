6ab6b48a565d143440e2944e451f1c8d59b996e6
Briws <brightwiths@gmail.com>
2024-01-23 02:51:16 +0700

    changed embed to youtube-nocookie.com for privacy

--- src/common/MyYouTube.tsx
+++ src/common/MyYouTube.tsx
@@ -3,7 +3,7 @@ export const MyYouTube = (props: Props) => {
     return <iframe
         width={480}
         height={270}
-        src={`https://www.youtube.com/embed/${props.videoId}`}
+        src={`https://www.youtube-nocookie.com/embed/${props.videoId}`}
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
     />
