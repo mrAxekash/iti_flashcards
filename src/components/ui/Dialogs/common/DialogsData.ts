d546c7553efb9b8e66b6fb171f7a9c2e60cdca5f
Briws <brightwiths@gmail.com>
2024-01-24 01:40:28 +0700

    begin, set min slider value 1

--- src/components/ui/Dialogs/common/DialogsData.ts
+++ src/components/ui/Dialogs/common/DialogsData.ts
@@ -1,5 +1,5 @@
 export const canvaWidth = 484
 export const canvaHeight = 119
-export const minSliderValue = 4
+export const minSliderValue = 1
 export const maxSliderValue = 15
 export const sliderStep = 1
