19e7fb39242fa36b1f6eb019315786e106baf093
Briws <brightwiths@gmail.com>
2024-03-13 19:12:39 +0700

    Added clean the old YouTube answer and question

--- src/components/ui/Dialogs/DialogAddCard/DialogAddCard.tsx
+++ src/components/ui/Dialogs/DialogAddCard/DialogAddCard.tsx
@@ -58,6 +58,11 @@ export const DialogAddCard = (props: Props) => {
 
     const onAddNewCard = async (question: string, answer: string) => {
         if (!question || !answer || !props.deckId) return
+
+        // clean old values
+        setYoutubeQuestionUrl('')
+        setYoutubeAnswerUrl('')
+
         const formData = new FormData()
         const questionImg = await fromBase64(cropQuestionImg ? cropQuestionImg : '')
         const answerImg = await fromBase64(cropAnswerImg ? cropAnswerImg : '')
